0d4145f1ab38416dd9620d72b93de7516ec195a3
dlshoong <dlshoong@users.noreply.github.com>
2017-05-12 11:54:05 +0800

    Update app.js

--- Node/intelligence-LUIS/app.js
+++ Node/intelligence-LUIS/app.js
@@ -28,7 +28,7 @@ var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
 bot.dialog('GetUserLocation', [
-    function (session){
+    function (session, args){
         builder.Prompts.text(session, "Send me your current location.");
     },
     function (session) {
@@ -42,6 +42,62 @@ bot.dialog('GetUserLocation', [
     }
 ]).triggerAction({
     matches: 'GetUserLocation'
+});
+
+bot.dialog('SearchHotels', [
+    function (session, args, next) {
+        session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
+
+        // try extracting entities
+        var cityEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
+        var airportEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'AirportCode');
+        if (cityEntity) {
+            // city entity detected, continue to next step
+            session.dialogData.searchType = 'city';
+            next({ response: cityEntity.entity });
+        } else if (airportEntity) {
+            // airport entity detected, continue to next step
+            session.dialogData.searchType = 'airport';
+            next({ response: airportEntity.entity });
+        } else {
+            // no entities detected, ask user for a destination
+            builder.Prompts.text(session, 'Please enter your destination');
+        }
+    },
+    function (session, results) {
+        var destination = results.response;
+
+        var message = 'Looking for hotels';
+        if (session.dialogData.searchType === 'airport') {
+            message += ' near %s airport...';
+        } else {
+            message += ' in %s...';
+        }
+
+        session.send(message, destination);
+
+        // Async search
+        Store
+            .searchHotels(destination)
+            .then(function (hotels) {
+                // args
+                session.send('I found %d hotels:', hotels.length);
+
+                var message = new builder.Message()
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(hotels.map(hotelAsAttachment));
+
+                session.send(message);
+
+                // End
+                session.endDialog();
+            });
+    }
+]).triggerAction({
+    matches: 'SearchHotels',
+    onInterrupted: function (session) {
+        session.send('Please provide a destination');
+    }
 });
 
 bot.dialog('ShowHotelsReviews', function (session, args) {
