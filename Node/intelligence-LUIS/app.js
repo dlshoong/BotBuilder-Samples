7abadb63e12af1efedb21b8479d74a9681d94467
dlshoong <dlshoong@users.noreply.github.com>
2017-05-12 18:47:12 +0800

    Update app.js

--- Node/intelligence-LUIS/app.js
+++ Node/intelligence-LUIS/app.js
@@ -5,7 +5,8 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var Store = require('./store');
 var spellService = require('./spell-service');
-var locationDialog = require('botbuilder-location');
+//var locationDialog = require('./core/lib/botbuilder-location');
+var locationDialog = require('./core/lib/spell-service');
 
 // Setup Restify Server
 var server = restify.createServer();
