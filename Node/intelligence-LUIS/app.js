27364287bc686e94ec0a74d40993033dbaea4faf
dlshoong <dlshoong@users.noreply.github.com>
2017-05-15 11:48:54 +0800

    Update app.js

--- Node/intelligence-LUIS/app.js
+++ Node/intelligence-LUIS/app.js
@@ -50,18 +50,11 @@ bot.use({
                 // Echo back users text
                 session.send("You said: %s", session.message.text);
                 session.send("Type: %s", session.message.type);
+                session.send("Type: %s", session.message.attachment[0].contentType);
+                session.send("Type: %s", session.message.attachment[0].contentUrl)
+                session.send("Type: %s", session.message.attachment[0].name)
                 
-                var attachment = msg.attachments[0];
-                session.send({
-                    text: "You sent:",
-                    attachments: [
-                        {
-                            contentType: attachment.contentType,
-                            contentUrl: attachment.contentUrl,
-                            name: attachment.name
-                        }
-                    ]
-                });
+
             }
     }
 })
